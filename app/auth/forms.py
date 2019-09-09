7f79decfea348e6c3ca2825dcc7d9b86bf598018
williammango <williammango2015@gmail.com>
2019-09-09 20:04:10 +0300

    created admin register form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -10,3 +10,16 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators =[Required()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
+
+class AdminForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    name = StringField('Enter your name',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
+    def validate_email(self,data_field):
+        if Staff.query.filter_by(email =data_field.data).first():
+            raise ValidationError('There is an account with that email')
+    def validate_username(self,data_field):
+        if Staff.query.filter_by(name = data_field.data).first():
+            raise ValidationError('That name is taken')
