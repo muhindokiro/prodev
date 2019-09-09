e34a092cf2e22b30d9cd2fb7837f689b13401c64
williammango <williammango2015@gmail.com>
2019-09-09 20:17:21 +0300

    created general email confirations

--- config.py
+++ config.py
@@ -10,6 +10,13 @@ class Config:
     
 
     # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
    
     
 class ProdConfig(Config):
